ad0b7c809db8ceaac41897e806096a637f0664b7
Nadim Bhatti <bhattin@bzz.ch>
2023-02-08 13:29:31 +0100

    Count Person done

--- src/main/java/ch/bzz/timetracker/data/PersonDAO.java
+++ src/main/java/ch/bzz/timetracker/data/PersonDAO.java
@@ -1,4 +1,15 @@
 package ch.bzz.timetracker.data;
+
+import ch.bzz.timetracker.service.Config;
+
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
 /**
  * data access object for person
  * Time Tracker
@@ -12,11 +23,50 @@ public class PersonDAO {
      * @return
      */
     public Integer count() {
-        // TODO establish connection
+        Connection connection = null;                                         // import java.sql.Connection;
+        try {
+            InitialContext initialContext = new InitialContext();
+            // import javax.naming.InitialContext
+            DataSource dataSource = (DataSource) initialContext.lookup(Config.getProperty("jdbcRessource")); // import javax.sql.DataSource
+            connection = dataSource.getConnection();
 
-        // TODO count users in database
+        } catch (NamingException nameEx) {                                    // import javax.naming.NamingException
+            nameEx.printStackTrace();
+            throw new RuntimeException();
+        } catch (SQLException sqlEx) {                                        // import java.sql.SQLException
+            sqlEx.printStackTrace();
+            throw new RuntimeException();
+        }
 
-        return -93; // FIXME return the number of users
+        int countRows = 0;
+        PreparedStatement prepStmt = null;
+        ResultSet resultSet = null;
+        String sqlQuery = "SELECT COUNT(*) FROM Person";
+        try {
+            prepStmt = connection.prepareStatement(sqlQuery);
+            resultSet = prepStmt.executeQuery();
+
+            if (resultSet.next()) {                // resultSet contains rows
+                countRows = resultSet.getInt(1);   // get the value of the first attribute
+            }
+        } catch (SQLException sqlEx) {
+            sqlEx.printStackTrace();
+            throw new RuntimeException();
+        }  finally {
+            try {
+                if (resultSet != null)
+                    resultSet.close();
+                if (prepStmt != null)
+                    prepStmt.close();
+                if (connection != null)
+                    connection.close();
+            } catch (SQLException sqlEx) {
+                sqlEx.printStackTrace();
+                throw new RuntimeException();
+            }
+        }
+
+        return countRows;
 
     }
 }
